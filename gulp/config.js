585438f70969446916d4c25288fc5b7b2269cb85
Alexander Nikitin <nikitindiz@gmail.com>
2015-06-22 22:16:13 +0700

    Initial commit

--- gulp/config.js
+++ gulp/config.js
@@ -13,56 +13,31 @@ module.exports = {
     dest: dest,
     settings: {
       indentedSyntax: true, // Enable .sass syntax!
-      imagePath: 'images' // Used by the image-url helper
+      imagePath: 'img' // Used by the image-url helper
     }
   },
   images: {
-    src: src + "/images/**",
-    dest: dest + "/images"
+    src: src + "/img/**",
+    dest: dest + "/img"
   },
   markup: {
-    src: src + "/htdocs/**",
+    src: src + "/html/**",
     dest: dest
-  },
-  iconFonts: {
-    name: 'Gulp Starter Icons',
-    src: src + '/icons/*.svg',
-    dest: dest + '/fonts',
-    sassDest: src + '/sass',
-    template: './gulp/tasks/iconFont/template.sass.swig',
-    sassOutputName: '_icons.sass',
-    fontPath: 'fonts',
-    className: 'icon',
-    options: {
-      fontName: 'Post-Creator-Icons',
-      appendCodepoints: true,
-      normalize: false
-    }
   },
   browserify: {
     // A separate bundle will be generated for each
     // bundle config in the list below
     bundleConfigs: [{
-      entries: src + '/javascript/global.coffee',
+      entries: src + '/js/index.js',
       dest: dest,
-      outputName: 'global.js',
-      // Additional file extentions to make optional
-      extensions: ['.coffee', '.hbs'],
-      // list of modules to make require-able externally
-      require: ['jquery', 'backbone/node_modules/underscore']
-      // See https://github.com/greypants/gulp-starter/issues/87 for note about
-      // why this is 'backbone/node_modules/underscore' and not 'underscore'
-    }, {
-      entries: src + '/javascript/page.js',
-      dest: dest,
-      outputName: 'page.js',
+      outputName: 'index.js',
       // list of externally available modules to exclude from the bundle
       external: ['jquery', 'underscore']
     }]
   },
   production: {
-    cssSrc: dest + '/*.css',
-    jsSrc: dest + '/*.js',
+    cssSrc: dest + '/css/*.css',
+    jsSrc: dest + '/js/*.js',
     dest: dest
   }
 };
